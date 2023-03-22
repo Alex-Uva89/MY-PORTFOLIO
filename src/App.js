ed00ab46cfd6f8d7e75a1af201e89e95ae62513e
alessandro <auva@alpenite.com>
2023-03-22 10:52:03 +0100

    [COMMIT1] START

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import './scss/App.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>h1 stile</h1>
+      <header>
+          <h2>h2 stile</h2>
+          <h3></h3>
+          <h4></h4>
+          <h3></h3>
+          <h3></h3>
       </header>
+      <main>
+        
+      </main>
+      <aside>
+        
+      </aside>
+      <footer>
+        
+      </footer>
     </div>
   );
 }
